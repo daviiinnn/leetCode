16b180cf623afc155b986303f1f20f612f4c8f38
davin <dizqar@gmail.com>
2020-03-09 12:34:05 +0000

    Runtime: 128 ms, faster than 42.75% of JavaScript online submissions

--- 0004.js
+++ 0004.js
@@ -3,11 +3,7 @@
  * @param {number[]} nums2
  * @return {number}
  */
-var a = [1, 3]
-var b = [2]
-var c = [3]
-var d = [-2,-1]
-var fem = function(nums1, nums2) {
+var findMedianSortedArrays = function(nums1, nums2) {
     let sa = nums1.concat(nums2).sort(function(a,b){return a - b});
     if(sa.length % 2 === 0){
       return (sa[(sa.length / 2)] + sa[(sa.length / 2) -1]) /2;
