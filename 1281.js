d311ba44da19ba5b7e1982ea5d10f572746475b9
davin <dizqar@gmail.com>
2020-03-13 02:34:28 +0000

    Runtime: 56 ms, faster than 59.56% of JavaScript online submissions

--- 1281.js
+++ 1281.js
@@ -1,13 +1,8 @@
-var k = 234;
-var b = k.toString().split('').map((a)=> parseInt(a, 10));
-var kali = b.reduce((a,b)=> a*b);
-var kal = b.reduce((a,b)=> a+b);
-
 /**
  * @param {number} n
  * @return {number}
  */
-var subt = function(n) {
+var subtractProductAndSum = function(n) {
     const b = n.toString().split('').map((a)=> parseInt(a, 10));
     var multpl = b.reduce((a,b)=> a*b);
     var add = b.reduce((a,b)=> a+b);
